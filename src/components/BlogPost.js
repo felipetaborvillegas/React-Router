cadfdf412caf4cf2be30740461aad3dc5d20a52a
felipetaborvillegas <felipetaborvillegas@gmail.com>
2022-09-18 19:04:14 -0500

    fix typo

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -18,7 +18,7 @@ export function BlogPost () {
             <h2>{blogpost.title}</h2>
             <p>{blogpost.content}</p>
             <p>Died: {blogpost.died}</p>
-            <button onClick={returnToBlog}>Volver al blog</button>
+            <button onClick={returnToBlog}>Volver</button>
         </div>
     )
 }
